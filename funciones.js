2134c9937a2b4c47d66d374a310fabe97e4651d5
Mara gabriela Zumaeta Sánchez <marazumaeta@gmail.com>
2022-11-26 14:54:14 +0000

    feat () inclyendo nuevas funciones  y arreglos

--- funciones.js
+++ funciones.js
@@ -1,18 +1,18 @@
 
 const students = [{
-  age: 32,
+  age: 25,
   examScores: [],
   gender: 'male',
   name: 'edu'
 },
 {
-  age: 26,
+  age: 30,
   examScores: [],
   gender: 'male',
   name: 'juan'
 },
 {
-  age: 30,
+  age: 29,
   examScores: [],
   gender: 'male',
   name: 'victor'
@@ -36,20 +36,20 @@ const students = [{
   name: 'carlos'
 },
 {
-  age: 22,
+  age: 25,
   examScores: [],
   gender: 'female',
   name: 'silvia'
 },
 
 {
-  age: 45,
+  age: 30,
   examScores: [],
   gender: 'female',
   name: 'virginia'
 },
 {
-  age: 38,
+  age: 29,
   examScores: [],
   gender: 'female',
   name: 'isabel'
@@ -60,6 +60,7 @@ const students = [{
   gender: 'female',
   name: 'luisa'
 },
+
 {
   age: 35,
   examScores: [],
@@ -74,24 +75,24 @@ const students = [{
 },
 ]
 
-/*const availableMaleNames = ['pepe', 'juan', 'victor', 'Leo', 'francisco', 'carlos'];
+const availableMaleNames = ['pepe', 'juan', 'victor', 'Leo', 'francisco', 'carlos'];
 const availableFemaleNames = ['cecilia', 'ana', 'luisa', 'silvia', 'isabel', 'virginia'];
-const availableGenders = ['male', 'female'];*/
+const availableGenders = ['male', 'female'];
 
 /*Mis funciones*/
 //case 1
-export function mostrarTabla() {
+export function showTable() {
   console.table(students);
 }
 //case2
-export function cantidadAlumnos() {
+export function numberStudents() {
 
-  console.log('\n Hay', students.length, "Alumnos en clase");
+  console.log('\n There are', students.length, " students");
 
 }
 //case 3
 
-export function nameAlumnos() {
+export function nameStudents() {
   const student = students.map(function(allNames) {
     return allNames.name;
   })
@@ -99,13 +100,13 @@ export function nameAlumnos() {
 }
 
 //case 4
-export function removeAlumno() {
+export function removeStudent() {
   //Elimina el útimo alumno de la lista
-  console.log(students.pop());  
+  console.log(students.pop());
 }
 
 //case 5
-export function alumnRandom() {
+export function studentRandom() {
   const student = students[Math.floor(Math.random() * students.length)];
   //intentar comprender las funciones flechas
   const resultado = students.filter(studen => studen != student);
@@ -121,29 +122,85 @@ export function onlyGirls() {
 }
 
 //case 7
-export function cantidadPorGenero() {
+export function perGender() {
   const girls = students.filter(function(student) {
     return student.gender == 'female';
   });
-console.log(girls.length, "Chicas");
+  console.log(girls.length, "Chicas");
   const boys = students.filter(function(student) {
     return student.gender == 'male';
   });
-console.log(boys.length, "Chicos");
+  console.log(boys.length, "Chicos");
 }
 
 //case 8
-export function sonChicas() {
-  const student = students.map(el => el.gender);
-  console.log(student.includes("male")); 
+//revisar el ejercicio
+export function areGirls() {
+  const student = students.map(person => person.gender);
+  console.log(student.includes("male"));
 }
 
 //case 9
-export function entreEdades() {
-  const student = students.filter(el => el.age >= 20 && el.age <= 25);
-    console.log(student);
+export function calculateAge() {
+  const student = students.filter(person => person.age >= 20 && person.age <= 25);
+  console.log(student);
 }
 
 
 
+//me falta el número 10
+
+
+
+//case 11
+export function youngerAge() {
+  const lowest = students.reduce((previous, current) => {
+    return current.age < previous.age ? current : previous;
+  });
+  console.log('lowest', lowest);
+  //otra forma de hacerlo
+  //const lowest = students.reduce((prev, cur) => (cur.age < prev.age ? cur : prev));
+}
+
+//case 12
+//repasar la nota media no sé si funciona
+export function averageAge() {
+  function getAverageAge(students) {
+    return students.reduce((prev, user) => prev + user.age, 0) / students.length;
+  }
+
+  let c = getAverageAge(students);
+  let x = Math.round(c);
+
+  console.log("La edad media de los alumnos es:", x);
+
+}
+
+
+// case 13
+export function averageAgeWoman() {
+  const allNames = students.filter(function(student) {
+    return student.gender == 'female';
+  });
+  console.log(allNames);
+
+  function getAverageAge(allNames) {
+    return allNames.reduce((prev, user) => prev + user.age, 0) / allNames.length;
+  }
+
+  let c = getAverageAge(allNames);
+  let x = Math.round(c);
+
+  console.log("La edad media de los alumnas mujeres es:", x);
+}
+
+//ME FALTA EL CASE 14
+
+//case 15
+export function studentsAlphabetically() {
+  const student = students.map(function(allNames) {
+    return allNames.name;
+  })
+  console.log(student.sort());
+}
 
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*importamos nuestras funciones*/
 
-import { mostrarTabla, cantidadAlumnos, nameAlumnos, onlyGirls,removeAlumno, cantidadPorGenero, alumnRandom, sonChicas, entreEdades } from "./funciones.js";
-import {message} from "./text.js";
+import * as myFunction from './funciones.js';
+import { message } from "./text.js";
 import readline from "readline";
 
 /*configuramos la utilidad de node para que los datos se pidan y se muestren por consola.*/
@@ -27,13 +27,13 @@ const isInt2 = (str) => {
 // productor 
 function getNumberFromConsole() {
   const promise = new Promise((resolve, reject) => {
-    rl.question(" Introduce un número: ", (num) => {
+    rl.question("Please, enter one of the indicated options: ", (num) => {
       rl.pause();
       if (isInt(num)) {
         num = Number.parseInt(num);
         resolve(num);
       } else {
-        reject("Has de introducir un número");
+        reject("Please, You have to enter a number");
       }
     });
   });
@@ -41,108 +41,108 @@ function getNumberFromConsole() {
   return promise;
 }
 
+
 /*Arrancamos un bucle Do While para que el usuario pueda introducir los datos tantas veces como desee o pulsar un 0 u otro número para terminar*/
 
- let numberFromConsole;
-  let counter = 0;
+let numberFromConsole;
+let counter = 0;
 
- do {
-    try {
-      message();
-      counter++;
-      numberFromConsole = await getNumberFromConsole();
-      
-      
-    } catch (error) {
-      console.log(error);
-      process.exit(0);
-    }
-
-/*Creamos un Switch para que el usuario pueda evaluar el caso que quiera seleccionar */
-switch (numberFromConsole) {
-  case 1:
-  mostrarTabla()
-    break;
-
-  case 2:
-    cantidadAlumnos();
-    break;
-
-  case 3:
-    nameAlumnos();
-    break;
-
-  case 4:
-    removeAlumno();
-    break;
-
-  case 5:
-   alumnRandom();
-    break;
-
-  case 6:
-    onlyGirls();
-    break;
-
-  case 7:
-    cantidadPorGenero();
-    break;
-
-  case 8:
-    sonChicas();
-    break;
-    
-  case 9:
-    entreEdades();
-    break;
-    
-  case 10:
-    numberFromConsole == 2;
-    console.log("Número 3 en una cadena");
-    break;
-  case "11":
-    numberFromConsole == 2;
-    console.log("Número 3 en una cadena");
-    break;
-  case "12":
-    numberFromConsole == 2;
-    console.log("Número 3 en una cadena");
-    break;
-  case "13":
-    numberFromConsole == 2;
-    console.log("Número 3 en una cadena");
-    break;
-  case "14":
-    numberFromConsole == 2;
-    console.log("Número 3 en una cadena");
-    break;
-  case "15":
-    numberFromConsole == 2;
-    console.log("Número 3 en una cadena");
-    break;
-  case "16":
-    numberFromConsole == 2;
-    console.log("Número 3 en una cadena");
-    break;
-  case "17":
-    numberFromConsole == 2;
-    console.log("Número 3 en una cadena");
-    break;
-  case "18":
-    numberFromConsole == 2;
-    console.log("Número 3 en una cadena");
-    break;
+do {
+  try {
+    message();
+    counter++;
+    numberFromConsole = await getNumberFromConsole();
 
 
-  default:
+  } catch (error) {
+    console.log(error);
+    process.exit(0);
+  }
 
-   rl.close();
-    console.log("El programa ha terminado".close);
-}/*Evalumamos la condición del while */
-   } while (numberFromConsole !== 0 );
-     
+  /*Creamos un Switch para que el usuario pueda evaluar el caso que quiera seleccionar */
+  switch (numberFromConsole) {
+    case 1:
+      myFunction.showTable();
+      break;
+
+    case 2:
+      myFunction.numberStudents();
+      break;
+
+    case 3:
+      myFunction.nameStudents();
+      break;
+
+    case 4:
+      myFunction.removeStudent();
+      break;
+
+    case 5:
+      myFunction.studentRandom();
+      break;
+
+    case 6:
+      myFunction.onlyGirls();
+      break;
+
+    case 7:
+      myFunction.perGender();
+      break;
+
+    case 8:
+      myFunction.areGirls();
+      break;
+
+    case 9:
+      myFunction.calculateAge();
+      break;
+
+    case 10:
+      console.log("No se hacerlo")
+      break;
+
+    case 11:
+      myFunction.youngerAge();
+      break;
+
+    case 12:
+      myFunction.averageAge();
+      break;
+
+    case 13:
+      myFunction.averageAgeWoman();
+      break;
+
+    case 14:
+      console.log("No se hacerlo");
+      break;
+
+    case 15:
+      myFunction.studentsAlphabetically();
+      break;
+
+    case "16":
+      numberFromConsole == 2;
+      console.log("Número 3 en una cadena");
+      break;
+    case "17":
+      numberFromConsole == 2;
+      console.log("Número 3 en una cadena");
+      break;
+    case "18":
+      numberFromConsole == 2;
+      console.log("Número 3 en una cadena");
+      break;
 
 
-    
+    default:
+      rl.close();
+     /* console.log("El programa ha terminado".close);*/
+  }/*Evalumamos la condición del while */
+} while (numberFromConsole > 0 && numberFromConsole <= 18);
+
+
+
+
 
 
